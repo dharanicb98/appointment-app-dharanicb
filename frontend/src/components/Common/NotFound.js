d7ee510880758d5d939cd1ec34ea20234173545b
dharanicb98 <dharanidhamini123@gmail.com>
2024-06-27 13:20:53 +0530

    update frontend

--- frontend/src/components/Common/NotFound.js
+++ frontend/src/components/Common/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
         <div className="bg-transparent py-16 px-4 sm:px-6 lg:px-8">
             <div className="max-w-7xl mx-auto">
                 <div className="text-center">
-                    <h2 className="text-2xl font-bold text-gray-800">
+                    <h2 className="text-2xl font-bold text-white">
                         Page Not Found
                     </h2>
                 </div>
